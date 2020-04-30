d7d5ee62a9f1739e1d0a95c06924d14dee78e701
Angie Spears <angie.spears809@gmail.com>
2020-04-30 19:28:57 -0400

    create sidebar files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import MapView from './map/MapView'
+import Sidebar from './sidebar/sidebar'
 
 function App() {
   return (
-    <div>
+    <div className = 'app-container'>
+      <Sidebar/>
       <MapView/>
-       {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-  </header> */}
-
     </div>
   );
 }
---
+++ my-app/src/sidebar/sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sidebar.css'
+
+const sidebar = () => {
+  return (
+    <div className = 'sidebar-container'>
+      <div>Sidebar goes here</div>
+    </div>
+  )
+}
+
+
+export default sidebar
