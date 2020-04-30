f81667c957aa8561a4e3f2b71a129cab88210659
Angie Spears <angie.spears809@gmail.com>
2020-04-30 18:51:47 -0400

    add mapbox

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import MapView from './map/MapView'
+
+function App() {
+  return (
+    <div>
+      <MapView/>
+       {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+  </header> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/map/MapView.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import mapboxgl from 'mapbox-gl'
+import './mapView.css'
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
+
+class MapView extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    }
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: 'map-container',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    })
+
+    map.on('move', () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(2),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='sidebarStyle'>
+          <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+        </div>
+        <div id = 'map-container'></div>
+      </div>
+    )
+  }
+}
+
+
+
+export default MapView
