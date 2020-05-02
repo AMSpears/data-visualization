da49ab5ed635574e1c71cd9430cd723d13c3a2ad
Angie Spears <angie.spears809@gmail.com>
2020-05-02 18:12:09 -0400

    add initial stats to map

--- my-app/src/map/MapView.js
+++ my-app/src/map/MapView.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import ReactMapGL, { Marker} from "react-map-gl";
+import React, { useState, useEffect} from "react";
+import ReactMapGL, { Marker, Popup} from "react-map-gl";
 import * as data from "../data/data.json";
 import './mapView.css'
 
@@ -11,6 +11,22 @@ export const MapView = () => {
     height: "100vh",
     zoom: 2
   })
+
+  const [selectedState, setSelectedState] = useState(null)
+
+  useEffect( () => {
+    const listener = evt => {
+      if (evt.key === 'Escape') {
+        setSelectedState(null)
+      }
+
+      window.addEventListener("keydown", listener)
+
+      return () => {
+        window.removeEventListener("keydown", listener)
+      }
+    }
+  }, [])
 
   return (
     <div>
@@ -31,12 +47,30 @@ export const MapView = () => {
             >
             <button
               className="marker-btn"
+               onClick={e => {
+                e.preventDefault();
+                setSelectedState(place);
+              }}
             >
               <img src="/location-pin.svg" alt="City" />
             </button>
           </Marker>
-         ))
-      }
+         ))}
+          {selectedState ? (
+          <Popup
+            latitude={selectedState.latitude}
+            longitude={selectedState.longitude}
+            onClose={() => {
+              setSelectedState(null);
+            }}
+          >
+            <div>
+              <h2> Stats:</h2>
+              <p> Cases: {selectedState.casesReported} </p>
+              <p> Range: {selectedState.range}</p>
+            </div>
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
